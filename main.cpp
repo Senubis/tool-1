9ec0cd5d8fbb5ee1695288b5d6f420561c44c7f5
Senubis <2492099735@qq.com>
2025-05-14 09:23:29 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,1 @@
-#include "mainwindow.h"
-#include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    QApplication::setStyle("Fusion");
-    MainWindow w;
-    w.setWindowTitle("番茄钟");
-    w.show();
-
-    return a.exec();
-}
